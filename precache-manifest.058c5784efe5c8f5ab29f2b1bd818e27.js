6a5ab7cb0e43149fabcb4075dbd7eb002adfdb9f
Larry Wu <larrywu@Larrys-MacBook-Pro.local>
2023-01-13 18:06:20 -0500

    Updates

---
+++ precache-manifest.058c5784efe5c8f5ab29f2b1bd818e27.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0ea9d1877076ba219d0e14b032c034",
+    "url": "/larrythwu.com/index.html"
+  },
+  {
+    "revision": "b23ac57ea75d58763351",
+    "url": "/larrythwu.com/static/css/main.7ce91f25.chunk.css"
+  },
+  {
+    "revision": "865f481ae42e8c2cdbd4",
+    "url": "/larrythwu.com/static/js/2.b388090c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/larrythwu.com/static/js/2.b388090c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23ac57ea75d58763351",
+    "url": "/larrythwu.com/static/js/main.fed7dbea.chunk.js"
+  },
+  {
+    "revision": "e306b55cd532f22cc9be",
+    "url": "/larrythwu.com/static/js/runtime-main.bba864ae.js"
+  },
+  {
+    "revision": "c1973b662757503cf762ae7c836a9fa0",
+    "url": "/larrythwu.com/static/media/DarkMode.c1973b66.png"
+  },
+  {
+    "revision": "b65039c8536b7be26f3fea01d0e6b046",
+    "url": "/larrythwu.com/static/media/Description.b65039c8.png"
+  },
+  {
+    "revision": "ba95fb21653e338153c5668b731a7b5a",
+    "url": "/larrythwu.com/static/media/FAT.ba95fb21.png"
+  },
+  {
+    "revision": "f3c76e87ef788b2201802904715463d4",
+    "url": "/larrythwu.com/static/media/GameControl.f3c76e87.png"
+  },
+  {
+    "revision": "dfe61b6b55fbe12d9b761b80749df289",
+    "url": "/larrythwu.com/static/media/GameScreenshot.dfe61b6b.png"
+  },
+  {
+    "revision": "99cd307c723ce1596cf0e1eb352fe1a9",
+    "url": "/larrythwu.com/static/media/IDT.99cd307c.png"
+  },
+  {
+    "revision": "ae47c80b36323deca639420afcef0311",
+    "url": "/larrythwu.com/static/media/IMG1.ae47c80b.jpg"
+  },
+  {
+    "revision": "b93d4f51a6e508809d7bcbe8b9aad4e1",
+    "url": "/larrythwu.com/static/media/IMG10.b93d4f51.jpg"
+  },
+  {
+    "revision": "b22c020891ceed8f3104d8784fc23ea4",
+    "url": "/larrythwu.com/static/media/IMG11.b22c0208.jpg"
+  },
+  {
+    "revision": "74b91160c4f3d24092900ac8ec089e12",
+    "url": "/larrythwu.com/static/media/IMG2.74b91160.jpg"
+  },
+  {
+    "revision": "1445b07bcf0a53f73af3fb3072bb38ac",
+    "url": "/larrythwu.com/static/media/IMG3.1445b07b.jpg"
+  },
+  {
+    "revision": "dc316d45f98e77c4af33f3eba8f7928c",
+    "url": "/larrythwu.com/static/media/IMG4.dc316d45.jpg"
+  },
+  {
+    "revision": "eced526fee2953ba479475b281357450",
+    "url": "/larrythwu.com/static/media/IMG5.eced526f.jpg"
+  },
+  {
+    "revision": "91b76ed51e3657232eef69c173a47461",
+    "url": "/larrythwu.com/static/media/IMG6.91b76ed5.jpg"
+  },
+  {
+    "revision": "2f56c6b7cfd5990c3e0232cafc6b876c",
+    "url": "/larrythwu.com/static/media/IMG7.2f56c6b7.jpg"
+  },
+  {
+    "revision": "388cdc84fb1e12fa93714d245bdf8a5f",
+    "url": "/larrythwu.com/static/media/IMG8.388cdc84.jpg"
+  },
+  {
+    "revision": "e8a98d550a847e6547650467505168eb",
+    "url": "/larrythwu.com/static/media/IMG9.e8a98d55.jpg"
+  },
+  {
+    "revision": "289603381de8e221202e061c8fbc68c4",
+    "url": "/larrythwu.com/static/media/LiveWeather.28960338.png"
+  },
+  {
+    "revision": "a0a6b6750c6f1d05d96cb12f1d66a339",
+    "url": "/larrythwu.com/static/media/MyMap.a0a6b675.png"
+  },
+  {
+    "revision": "acc4460e3c4033185346b66a526576c4",
+    "url": "/larrythwu.com/static/media/MyPic.acc4460e.JPG"
+  },
+  {
+    "revision": "63857a1dc9f30d84ff6d8aa7f5991dd0",
+    "url": "/larrythwu.com/static/media/Photo1.63857a1d.jpg"
+  },
+  {
+    "revision": "44e66b0015eeba54c95273ef7a79e3ff",
+    "url": "/larrythwu.com/static/media/Photo2.44e66b00.jpg"
+  },
+  {
+    "revision": "013ed097178f103e02cbdaaed4b52fd9",
+    "url": "/larrythwu.com/static/media/Photo3.013ed097.jpg"
+  },
+  {
+    "revision": "12deef7dca0cf16dfcd20971d92e74cd",
+    "url": "/larrythwu.com/static/media/Photo4.12deef7d.jpg"
+  },
+  {
+    "revision": "1c1effc7794a2b733987f2373ba0f0a7",
+    "url": "/larrythwu.com/static/media/Photo5.1c1effc7.jpg"
+  },
+  {
+    "revision": "2eb6d62feed70cd304aaea7f67e76cb2",
+    "url": "/larrythwu.com/static/media/Photo6.2eb6d62f.jpg"
+  },
+  {
+    "revision": "19da5d200f11765632903933f9b29c2b",
+    "url": "/larrythwu.com/static/media/Photo7.19da5d20.jpg"
+  },
+  {
+    "revision": "2ea69f9b699bf9215b54dd837e6c22d4",
+    "url": "/larrythwu.com/static/media/SearchSuggestions.2ea69f9b.png"
+  },
+  {
+    "revision": "dfc406a435e14ae209b6dfc408a03391",
+    "url": "/larrythwu.com/static/media/amd_icon.dfc406a4.png"
+  },
+  {
+    "revision": "dffbcf801a28f1cfa683023187a60a7d",
+    "url": "/larrythwu.com/static/media/bootloader.dffbcf80.png"
+  },
+  {
+    "revision": "c73dfa3f2f977cb2913004f16efcef10",
+    "url": "/larrythwu.com/static/media/c-plus-plus-logo.c73dfa3f.png"
+  },
+  {
+    "revision": "f6a9014d94108ad26729027d030cec0f",
+    "url": "/larrythwu.com/static/media/cuda_icon.f6a9014d.webp"
+  },
+  {
+    "revision": "4924a1358965c0996e75f225bfb4e042",
+    "url": "/larrythwu.com/static/media/docker_icon.4924a135.webp"
+  },
+  {
+    "revision": "95eb4b3e718e2cce6ec45035689a40ca",
+    "url": "/larrythwu.com/static/media/heap.95eb4b3e.png"
+  },
+  {
+    "revision": "1e1ba20cfa4a4c86d66437632e35df2f",
+    "url": "/larrythwu.com/static/media/java_icon.1e1ba20c.png"
+  },
+  {
+    "revision": "b6b59d8ce859b5c3fffa32b6771d5fde",
+    "url": "/larrythwu.com/static/media/llvm_icon.b6b59d8c.png"
+  },
+  {
+    "revision": "b4285fe102e75af1b31293299e50a0e7",
+    "url": "/larrythwu.com/static/media/node_js_icon.b4285fe1.png"
+  },
+  {
+    "revision": "53c3959f9df0cc28de869a7bc1951b47",
+    "url": "/larrythwu.com/static/media/paging.53c3959f.png"
+  },
+  {
+    "revision": "7d08e12830eb2a3923289c19dc35b9c4",
+    "url": "/larrythwu.com/static/media/perl_icon.7d08e128.webp"
+  },
+  {
+    "revision": "27ee5c95470eb5fff6bea4e1c0bb2122",
+    "url": "/larrythwu.com/static/media/piston1.27ee5c95.jpg"
+  },
+  {
+    "revision": "2361fb432e10958b278d2746b9bd839d",
+    "url": "/larrythwu.com/static/media/piston2.2361fb43.jpg"
+  },
+  {
+    "revision": "14eb65fefa07b39d1b3730ea0ce1a67a",
+    "url": "/larrythwu.com/static/media/piston3.14eb65fe.jpg"
+  },
+  {
+    "revision": "1e6e1c8544187aab698bd4d386dafe4c",
+    "url": "/larrythwu.com/static/media/psql_icon.1e6e1c85.png"
+  },
+  {
+    "revision": "433922316359bba377c77f63eaae9df5",
+    "url": "/larrythwu.com/static/media/python.43392231.png"
+  },
+  {
+    "revision": "193ad2d75c942f836cd8786f044adbcc",
+    "url": "/larrythwu.com/static/media/react_icon.193ad2d7.png"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/larrythwu.com/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "3362d30abddc6b94850cf3cfed84eefc",
+    "url": "/larrythwu.com/static/media/sumo1.3362d30a.jpg"
+  },
+  {
+    "revision": "6215315ddb0f9fdc66219f1a378e0597",
+    "url": "/larrythwu.com/static/media/sumo2.6215315d.jpg"
+  },
+  {
+    "revision": "0357c3512407e3bd89e7caa70499394b",
+    "url": "/larrythwu.com/static/media/sumo3.0357c351.jpg"
+  },
+  {
+    "revision": "27673bc18cd3ffca91c82c1d72dce489",
+    "url": "/larrythwu.com/static/media/traffic1.27673bc1.jpg"
+  },
+  {
+    "revision": "be0fcce2a71136277e280f5f99219843",
+    "url": "/larrythwu.com/static/media/traffic2.be0fcce2.jpg"
+  },
+  {
+    "revision": "b02b8e40409fcdad5cb3408d48ebb2e1",
+    "url": "/larrythwu.com/static/media/traffic3.b02b8e40.jpg"
+  },
+  {
+    "revision": "9d8b1609ee23415b5ae9f1c5aebcbc68",
+    "url": "/larrythwu.com/static/media/uoft_icon.9d8b1609.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Homepage/precache-manifest.55e9564e2036b02480675e5230525ab2.js"
+  "/larrythwu.com/precache-manifest.058c5784efe5c8f5ab29f2b1bd818e27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/larrythwu.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
